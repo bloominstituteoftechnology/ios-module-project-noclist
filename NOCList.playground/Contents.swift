61de8c7b617adfd57076769af535ae998d40b4d7
Chris Hittlet <Chrishittlet@gmail.com>
2019-09-04 21:23:17 -0700

    Step 10 completed

--- NOCList.playground/Contents.swift
+++ NOCList.playground/Contents.swift
@@ -85,22 +85,46 @@ print("\(findCleanAgents()) clean agents out of \(agentsArray.count) total agent
 //: ## Step 7
 //: Create a function called "findHighRisk" that prints out the real names and access levels of agents with level 8 or higher. If one of these agents is also currently compromised, add `**WARNING** **COMPROMISED**` to the end of the string that includes their name and access level.
 //: - Example: `Jon Voight, level: 9 **WARNING** **COMPROMISED**`
+func findHighRisk() {
+    for agents in agentsArray {
+        if agents.accessLevel >= 8 && agents.compromised == false {
+            print("\(agents.realName) is level \(agents.accessLevel)")
+        } else if  agents.accessLevel >= 8 {
+            print("\(agents.realName) is level \(agents.accessLevel) **WARNING** **COMPROMISED**")
+        }
+    }
+}
 
 
 
 //: ## Step 8
 //: Call the above function and check the output in the console to ensure it is functioning properly.
-
+findHighRisk()
 
 
 //: ## Step 9
 //: Create a function that finds totals for low, mid, and high level agents. Low level agents are 4 or lower, mid are 5-7, and high level agents are 8 or above. Iterate over each agent and use a `switch` statement to determine their level group. At the end of the function, print a statement like the following: "# low level agents, # mid level agents, and # high level agents"
-
-
-
+func agentsLevel() {
+    var lowLevel = 0
+    var midLevel = 0
+    var highLevel = 0
+    for agents in agentsArray{
+        switch agents.accessLevel {
+        case 1...4:
+            lowLevel += 1
+        case 5...7:
+            midLevel += 1
+        default:
+            highLevel += 1
+        }
+    }
+    print("\(lowLevel) agents are low level")
+    print("\(midLevel) agents are mid level")
+    print("\(highLevel) agents are high level")
+}
 //: ## Step 10
 //: Call the above function and check its output in the console.
-
+agentsLevel()
 
 
 //: ## Step 11 (Optional)

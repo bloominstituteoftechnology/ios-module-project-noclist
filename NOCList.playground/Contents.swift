a5bba2aba969c46e34fef167fee8ddcb43d3e313
jayfenam <jonathanferrer95@gmail.com>
2019-09-04 21:19:48 -0400

    Added a comment

--- NOCList.playground/Contents.swift
+++ NOCList.playground/Contents.swift
@@ -30,6 +30,7 @@ This message will self destruct in 5 seconds.
 //: ## Step 1
 //: Create constants for each of the above agents and store all their information in a tuple.
 print("hello world")
+//I am going to show you this
 
 //: ## Step 2
 //: Place the above constants inside an array. Declare this array as a constant as well.

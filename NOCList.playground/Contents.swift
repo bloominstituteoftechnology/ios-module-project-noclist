79339d13dcdec37e6b154e1e2c0325109c9bcbf7
Sam Gutierrez <samgutierrez@Sams-MacBook-Pro.local>
2019-09-04 19:49:47 -0600

    Added first agent tupel

--- NOCList.playground/Contents.swift
+++ NOCList.playground/Contents.swift
@@ -29,7 +29,7 @@ This message will self destruct in 5 seconds.
 */
 //: ## Step 1
 //: Create constants for each of the above agents and store all their information in a tuple.
-
+let Agent1 = (coverName: "Ethan Hunt", realName: "Tom Cruise", accessLevel: 8, compromised: false)
 
 
 //: ## Step 2

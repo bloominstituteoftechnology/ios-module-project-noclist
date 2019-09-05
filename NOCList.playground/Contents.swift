b5ed697ed77c1711b18f33d979a4c198b0b7e2b9
hectorsvill <hectorstevenvillasano@gmail.com>
2019-09-04 19:24:56 -0700

    message here

--- NOCList.playground/Contents.swift
+++ NOCList.playground/Contents.swift
@@ -29,6 +29,16 @@ This message will self destruct in 5 seconds.
 */
 //: ## Step 1
 //: Create constants for each of the above agents and store all their information in a tuple.
+dsds
+
+
+
+
+
+
+
+
+
 
 
 

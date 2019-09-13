99507c41cef0b1ebe46114fc25f746cf180efee4
Bree <bree.j.32@gmail.com>
2019-09-13 16:56:49 -0400

    working on steps 3, 4, and 5 not functional just committing changes

--- NOCList.playground/Contents.swift
+++ NOCList.playground/Contents.swift
@@ -45,28 +45,27 @@ let agentEleven = (coverName: "Frank Barnes", realName: "Dale Dye", accessLevel:
 //: Place the above constants inside an array. Declare this array as a constant as well.
 let agentList = [agentOne, agentTwo, agentThree, agentFour, agentFive, agentSix, agentSeven, agentEight, agentNine, agentTen, agentEleven]
 
-
 //: ## Step 3
 //: Create a function that calculates the total number of compromised agents. Inside the function, iterate over the array of agents to determine which ones are compromised. Return the total count.
-func compromisedList(compromised: Bool) -> Bool {
-    if compromised == true {
-        return agentList[].count
+func compromisedList() -> Int {
+    var compromisedListCount = 0
+    for compromised in agentList(_ compromised) {
+        if compromised {
+            compromisedListCount += 1
+        }
+    }
 }
 
 //: ## Step 4
 //: Call the above function to find the total number of compromised agents and then print a sentence that says "# agents have been compromised!" using string interpolation.
-
+compromisedList()
 
 
 //: ## Step 5
 //: Create a function called "findCleanAgents" that both prints the cover names of all uncompromised agents, as well as returns an array of agents that are uncompromised.
-    func findCleanAgents(compromised: Bool) -> Bool {
-        if compromised == false {
-            print(agentList[coverName])
-        }
+func findCleanAgents {
+    
 }
-
-
 //: ## Step 6
 //: Call the above function to find the total number of clean agents and print a message that says "# clean agents out of # total agents." Use the total number of agents in the array from step 2 as the second number in the string.
 
@@ -75,7 +74,9 @@ func compromisedList(compromised: Bool) -> Bool {
 //: ## Step 7
 //: Create a function called "findHighRisk" that prints out the real names and access levels of agents with level 8 or higher. If one of these agents is also currently compromised, add `**WARNING** **COMPROMISED**` to the end of the string that includes their name and access level.
 //: - Example: `Jon Voight, level: 9 **WARNING** **COMPROMISED**`
-
+func findHighRisk {
+    
+}
 
 
 //: ## Step 8

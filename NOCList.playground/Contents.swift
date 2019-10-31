667fcf85c93e8bdb21ec16f64cb2611bedc59f47
idelbiw <55222569+idelbiw@users.noreply.github.com>
2019-10-31 19:00:23 -0400

    finished step 10

--- NOCList.playground/Contents.swift
+++ NOCList.playground/Contents.swift
@@ -115,15 +115,42 @@ findHighRisk()
 
 //: ## Step 9
 //: Create a function that finds totals for low, mid, and high level agents. Low level agents are 4 or lower, mid are 5-7, and high level agents are 8 or above. Iterate over each agent and use a `switch` statement to determine their level group. At the end of the function, print a statement like the following: "# low level agents, # mid level agents, and # high level agents"
+var lowLevel = 0
+var midLevel = 0
+var highLevel = 0
+var whatIsThisLevel = 0
 
+func findLevels() {
+    for agent in agents {
+        switch agent.accessLevel {
+        case 1...4:
+            lowLevel += 1
+        case 5...7:
+            midLevel += 1
+        case 8...:
+            highLevel += 1
+            
+        default:
+            whatIsThisLevel += 1
+        }
+    }
+print("\(lowLevel) low level agents")
+print("\(midLevel) mid level agents")
+print("\(highLevel) high level agents")
+}
 
 
 //: ## Step 10
 //: Call the above function and check its output in the console.
 
-
+findLevels()
 
 //: ## Step 11 (Optional)
 //: Create and call a function that prints the cover names and access levels of all agents, but the list should be sorted by access level, in ascending order.
 
-
+func listAgents() {
+    for agent in agents {
+        print(agent.coverName, agent.accessLevel)
+    }
+}
+listAgents()

ac01a515455550d47d82f767756169c78eadcf75
alfredo <alfredo@alfredos-MBP.hitronhub.home>
2019-10-30 21:02:16 -0700

    test

--- NOCList.playground/Contents.swift
+++ NOCList.playground/Contents.swift
@@ -29,7 +29,7 @@ This message will self destruct in 5 seconds.
 */
 //: ## Step 1
 //: Create constants for each of the above agents and store all their information in a tuple.
-
+test
 
 
 //: ## Step 2

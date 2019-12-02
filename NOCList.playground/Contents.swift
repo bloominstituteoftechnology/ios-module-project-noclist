0acdbb4523d9a0cdc970d530b4d2c6eedcdd5c55
Kenny Dubroff <kennydubroff@hazystudios.com>
2019-12-02 12:27:21 -0800

    Formatted Console Output

--- NOCList.playground/Contents.swift
+++ NOCList.playground/Contents.swift
@@ -29,7 +29,6 @@ This message will self destruct in 5 seconds.
 */
 //: ## Step 1
 //: Create constants for each of the above agents and store all their information in a tuple.
-
 let ethanHunt = (coverName: "Ethan Hunt", realName: "Tom Cruise", accessLevel: 8, compromised: false)
 let jimPhelps = (coverName: "Jim Phelps", realName: "Jon Voight", accessLevel: 9, compromised: true)
 let clairePhelps = (coverName: "Claire Phelps", realName: "Emmanuelle Beart", accessLevel: 5, compromised: false)
@@ -45,8 +44,6 @@ let frankBarnes = (coverName: "Frank Barnes", realName: "Dale Dye", accessLevel:
 //: ## Step 2
 //: Place the above constants inside an array. Declare this array as a constant as well.
 let agentArr = [ethanHunt, jimPhelps, clairePhelps, eugeneKittridge, franzKrieger, lutherStickell, sarahDavies, maxRotGrab, hannahWilliams, jackHarmon, frankBarnes]
-
-
 //: ## Step 3
 //: Create a function that calculates the total number of compromised agents. Inside the function, iterate over the array of agents to determine which ones are compromised. Return the total count.
 func numAgentsCompromised() -> Int {
@@ -58,13 +55,11 @@ func numAgentsCompromised() -> Int {
     }
     return count
 }
-
-
 //: ## Step 4
 //: Call the above function to find the total number of compromised agents and then print a sentence that says "# agents have been compromised!" using string interpolation.
 let numCompromised = numAgentsCompromised()
 print("\(numCompromised) agents have been compromised!")
-
+print()
 
 //: ## Step 5
 //: Create a function called "findCleanAgents" that both prints the cover names of all uncompromised agents, as well as returns an array of agents that are uncompromised.
@@ -77,6 +72,7 @@ func findCleanAgents() -> [Any] {
             print("Agent: \(agent.coverName) is clean!")
         }
     }
+    print()
     return cleanAgents
 }
 
@@ -84,7 +80,7 @@ func findCleanAgents() -> [Any] {
 //: ## Step 6
 //: Call the above function to find the total number of clean agents and print a message that says "# clean agents out of # total agents." Use the total number of agents in the array from step 2 as the second number in the string.
 let cleanAgents = findCleanAgents()
-print("\(cleanAgents.count) clean agents out of \(agentArr.count)")
+print("\(cleanAgents.count) clean agents out of \(agentArr.count)\n")
 
 //: ## Step 7
 //: Create a function called "findHighRisk" that prints out the real names and access levels of agents with level 8 or higher. If one of these agents is also currently compromised, add `**WARNING** **COMPROMISED**` to the end of the string that includes their name and access level.
@@ -100,6 +96,7 @@ func findHighRisk() {
             print(printStatement)
         }
     }
+    print()
 }
 
 
@@ -128,7 +125,7 @@ func agentLevel() {
             print("\(agent.coverName) has an invalid access level.")
         }
     }
-    print("\(lowLevelAgents.count) low level agents, \(midLevelAgents.count) mid level agents, and \(highLevelAgents.count) high level agents")
+    print("\(lowLevelAgents.count) low level agents, \(midLevelAgents.count) mid level agents, and \(highLevelAgents.count) high level agents\n")
 }
 //: ## Step 10
 //: Call the above function and check its output in the console.

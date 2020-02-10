f6d83957243563e7fd534ab855de158925fc7fa8
Hector L <hectoremil21@hotmail.com>
2020-02-10 17:35:43 -0500

    Finished all of the requirements as well as the Bonus function. My logic is as follows:
    - Place the passed in array into a mutable copy
    - Start with first item of the array
    - Start with a separate variable index
    
    -Iterate through array (had to use i and .count, as passing in the value directly was not working out) until we find an item with lower level
    	- If we find an agent with lower level, change variable to hold this agent
    
    - After we go through the entire array:
    	-Print the agent info we were left with
    	-Remove said agent from the array
    	-Check if we have any agents left in the array and if we do:
    		- Reset the variable that holds the lowest level agent back to first item in the array
    		- Reset the value that holds our index back to 0

--- NOCList.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
+++ NOCList.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -23,13 +23,98 @@ func compAgents (_ list: [(coverName: String, realName: String, accessLevel: Int
             print("Agent \(agent.coverName) is compromised.")
         }
     }
-    print("\(compromisedAgents) agents are compromised.")
+    
     return compromisedAgents
 }
 
-func findCleanAgents (_ list: [(coverName: String, realName: String, accessLevel: Int, compromised: Bool)]) -> [()] {
+func findCleanAgents (_ list: [(coverName: String, realName: String, accessLevel: Int, compromised: Bool)]) -> [(coverName: String, realName: String, accessLevel: Int, compromised: Bool)] {
+    
+    var cleanAgents: [(coverName: String, realName: String, accessLevel: Int, compromised: Bool)] = []
+    
+    for agent in list {
+        if !(agent.compromised) {
+            cleanAgents.append(agent)
+            print("Agent \(agent.coverName) is uncompromised.")
+        }
+    }
+    
+    return cleanAgents
+}
+
+func findHighRisk (_ list: [(coverName: String, realName: String, accessLevel: Int, compromised: Bool)]) {
+    
+    for agent in list {
+        var string = ""
+        
+        if agent.accessLevel > 7 {
+            string = "\(agent.realName), level \(agent.accessLevel)"
+            if agent.compromised {
+                string += " **WARNING** **COMPROMISED**"
+            }
+            print(string)
+        }
+    }
     
 }
 
+func agentLevels (_ list: [(coverName: String, realName: String, accessLevel: Int, compromised: Bool)]) {
+    var low: UInt = 0
+    var mid: UInt = 0
+    var high: UInt = 0
+    
+    for agent in list {
+        switch agent.accessLevel {
+        case 0...4:
+            low += 1
+        case 5...7:
+            mid += 1
+        default:
+            high += 1
 
-compAgents(nocList)
+        }
+    }
+    print("\(low) low level agents, \(mid) mid level agents, and \(high) high level agents.")
+}
+
+func levelOrder (_ list: [(coverName: String, realName: String, accessLevel: Int, compromised: Bool)]) {
+    var holder = list // Pass by value. This will have its content deleted overtime.
+    var next = holder[0] // Start with first item in the array
+    var index = 0 // This will let us delete the item after it's printed
+    
+
+    while holder.count > 0 { // For as long as our placeholder has any items in it, loop.
+        for i in 0..<holder.count {
+            // check if said item is smaller
+            if holder[i].accessLevel < next.accessLevel {
+                // If it is, make it the new agent. Do this until there is no smaller one
+                next = holder[i]
+                index = i
+            }
+            
+        }
+
+        print("\(next.coverName), level \(next.accessLevel)") // Print the agent we're left with
+        holder.remove(at: index) // Remove this agent from the holder or get ready for xcode to YELL at you.
+        if holder.count > 0 { // Check if items left
+          next = holder[0] // Reset next to index 0 of the array
+          index = 0 // Reset index back to 0
+        }
+        
+        
+    }
+    
+}
+
+print("\(compAgents(nocList)) agents are compromised.")
+
+findCleanAgents(nocList)
+
+print("\(findCleanAgents(nocList).count) clean agents out of \(nocList.count) agents.")
+
+findHighRisk(nocList)
+
+agentLevels(nocList)
+
+levelOrder(nocList)
+
+

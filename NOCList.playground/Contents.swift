5b2dfda5c4925249b6bafb9bea26c0a16d4737ea
James McDougall <jameswilliammcdougall@gmail.com>
2020-11-16 11:28:44 -0600

    Step 7 completed

--- NOCList.playground/Contents.swift
+++ NOCList.playground/Contents.swift
@@ -83,12 +83,18 @@ print("\(totalCleanAgents) clean agents out of \(agentArray.count) total agents"
 //: ## Step 7
 //: Create a function called "findHighRisk" that prints out the real names and access levels of agents with level 8 or higher. If one of these agents is also currently compromised, add `**WARNING** **COMPROMISED**` to the end of the string that includes their name and access level.
 //: - Example: `Jon Voight, level: 9 **WARNING** **COMPROMISED**`
-
+func findHighRisk() {
+    
+    for agent in agentArray {
+        if agent.accessLevel > 8 && agent.compromised == true {
+            print("\(agent.realName), level: \(agent.accessLevel) **WARNING** **COMPROMISED**")
+        }
+    }
+}
 
 
 //: ## Step 8
 //: Call the above function and check the output in the console to ensure it is functioning properly.
-
 
 
 //: ## Step 9

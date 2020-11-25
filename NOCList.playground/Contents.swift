16a4b83f00505ad9a151c927331be93a7d297647
brys <brys1saclaus2@gmail.com>
2020-11-25 13:35:58 -0800

    created a func to find compromised agents and print out count

--- NOCList.playground/Contents.swift
+++ NOCList.playground/Contents.swift
@@ -29,24 +29,52 @@ This message will self destruct in 5 seconds.
 */
 //: ## Step 1
 //: Create constants for each of the above agents and store all their information in a tuple.
-let greeting = "Hella World"
-print(greeting)
+struct agent {
+    var coverName: String
+    var realName: String
+    var accessLevel: Int
+    var compromised: Bool
+}
+
+let agent1 = agent(coverName: "Ethan Hunt", realName: "Tom Cruise", accessLevel: 8, compromised: false)
+let agent2 = agent(coverName: "Jim Phelps", realName: "Jon Voight", accessLevel: 9, compromised: true)
+
+// let agent3 = (coverName: "Claire Phelps", realName: "Emmanuelle Beart", accessLevel: 5, compromised: false)
+// let agent4 = (coverName: "Eugene Kittridge", realName: "Henry Czerny", accessLevel: 10, compromised: true)
+// let agent5 = (coverName: "Franz Krieger", realName: "Jean Reno", accessLevel: 4, compromised: false)
+// let agent6 = (coverName: "Luther Stickell", realName: "Ving Rhames", accessLevel: 4, compromised: false)
+// let agent7 = (coverName: "Sarah Davies", realName: "Kristin Scott Thomas", accessLevel: 5, compromised: true)
+// let agent8 = (coverName: "Max RotGrab", realName: "Vanessa Redgrave", accessLevel: 4, compromised: false)
+// let agent9 = (coverName: "Hannah Williams", realName: "Ingeborga Dapkūnaitė", accessLevel: 5, compromised: true)
+// let agent10 = (coverName: "Jack Harmon", realName: "Emilio Estevez", accessLevel: 6, compromised: true)
+// let agent11 = (coverName: "Frank Barnes", realName: "Dale Dye", accessLevel: 9, compromised: false)
 
 
 //: ## Step 2
 //: Place the above constants inside an array. Declare this array as a constant as well.
-
+let agentArray = [agent1, agent2]
 
 
 //: ## Step 3
 //: Create a function that calculates the total number of compromised agents. Inside the function, iterate over the array of agents to determine which ones are compromised. Return the total count.
+func findCompromisedAgentsCount() -> Int {
+    var count = 0
+    
+    for i in agentArray {
+        if i.compromised == true {
+            count += 1
+            print("The total number of compromised agents is: \(count)")
+        }
+    }
+    return count
+}
 
 
 
 //: ## Step 4
 //: Call the above function to find the total number of compromised agents and then print a sentence that says "# agents have been compromised!" using string interpolation.
 
-
+findCompromisedAgentsCount()
 
 //: ## Step 5
 //: Create a function called "findCleanAgents" that both prints the cover names of all uncompromised agents, as well as returns an array of agents that are uncompromised.

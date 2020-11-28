4fb5ccb0597ab9999160cdbe91028ab6a8dd0a6d
brys <brys1saclaus2@gmail.com>
2020-11-27 17:56:35 -0800

    Able to findAgentLevels

--- NOCList.playground/Contents.swift
+++ NOCList.playground/Contents.swift
@@ -125,12 +125,30 @@ findHighRisk()
 
 //: ## Step 9
 //: Create a function that finds totals for low, mid, and high level agents. Low level agents are 4 or lower, mid are 5-7, and high level agents are 8 or above. Iterate over each agent and use a `switch` statement to determine their level group. At the end of the function, print a statement like the following: "# low level agents, # mid level agents, and # high level agents"
+func findAgentLevels() {
+     var lowLevelAgents = 0
+     var midLevelAgents = 0
+     var highLevelAgents = 0
 
+     for i in agentArray {
+         switch i.accessLevel {
+         case 0...4:
+             lowLevelAgents += 1
+         case 5...7:
+             midLevelAgents += 1
+         case 8...10:
+             highLevelAgents += 1
+         default:
+             print("There are no agents that meet this criteria")
+         }
+     }
+     print("\(lowLevelAgents) low level agents, \(midLevelAgents) mid level agents, \(highLevelAgents) high level agents")
+ }
 
 
 //: ## Step 10
 //: Call the above function and check its output in the console.
-
+findAgentLevels()
 
 
 //: ## Step 11 (Optional)
